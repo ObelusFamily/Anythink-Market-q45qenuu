3286916f6c1b3d660536760fe041d9d390a932e2
Compdog Inc <66779418+Compdog-inc@users.noreply.github.com>
2022-11-12 23:09:49 +0000

    Add support of undefined image

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -49,7 +49,7 @@ ItemSchema.methods.toJSONFor = function (user) {
     slug: this.slug,
     title: this.title,
     description: this.description,
-    image: this.image.trim() || "/placeholder.png",
+    image: (this.image || "").trim() || "/placeholder.png",
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
     tagList: this.tagList,
