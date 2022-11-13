da1f3fd8962c76c1c5680ab5ebc0c759ba8572ed
Compdog Inc <66779418+Compdog-inc@users.noreply.github.com>
2022-11-13 05:05:03 +0000

    Hide input field

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -18,41 +18,43 @@ const Banner = (props) => {
           >
             A place to get
           </span>
-          <label className={`search-label ${showSearch ? "visible" : ""}`}>
-            <input
-              className="search-box"
-              id="search-box"
-              type="text"
-              placeholder="What is it that you truly desire?"
-              value={search}
-              onInput={(e) => {
-                setSearch(e.currentTarget.value);
-                if (
-                  e.currentTarget.value.length >= 3 ||
-                  e.currentTarget.value.length === 0
-                )
-                  props.onSearch(e.currentTarget.value, props.tab);
-              }}
-            />
-            <svg viewBox="0 0 512 512" width="25" className="search-icon">
-              <path
-                d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
-                fill="none"
-                stroke="currentColor"
-                strokeMiterlimit="10"
-                strokeWidth="52"
+          {showSearch ? (
+            <label className={`search-label ${showSearch ? "visible" : ""}`}>
+              <input
+                className="search-box"
+                id="search-box"
+                type="text"
+                placeholder="What is it that you truly desire?"
+                value={search}
+                onInput={(e) => {
+                  setSearch(e.currentTarget.value);
+                  if (
+                    e.currentTarget.value.length >= 3 ||
+                    e.currentTarget.value.length === 0
+                  )
+                    props.onSearch(e.currentTarget.value, props.tab);
+                }}
               />
-              <path
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                strokeWidth="52"
-                d="M338.29 338.29L448 448"
-              />
-            </svg>
-          </label>
-          <span> the cool stuff.</span>
+              <svg viewBox="0 0 512 512" width="25" className="search-icon">
+                <path
+                  d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeMiterlimit="10"
+                  strokeWidth="52"
+                />
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  strokeWidth="52"
+                  d="M338.29 338.29L448 448"
+                />
+              </svg>
+            </label>
+          ) : null}
+          <span class="stuff"> the cool stuff.</span>
         </div>
       </div>
     </div>
